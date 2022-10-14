c666ad6063f2b764e36f3ce963fd0779308dd3f5
mohfaaqih <102928474+mohfaaqih@users.noreply.github.com>
2022-10-14 15:57:11 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/taskone/MainActivity.kt
+++ app/src/main/java/com/example/taskone/MainActivity.kt
@@ -4,37 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    val btnHappy = findViewById<Button>(R.id.btn_happy)
-    val btnSad = findViewById<Button>(R.id.btn_sad)
-    val btnMad = findViewById<Button>(R.id.btn_mad)
+
     lateinit var etName : EditText
+    lateinit var btnHappy : Button
+    lateinit var btnSad : Button
+    lateinit var btnMad : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var result = intent.getStringExtra("result")
+        etName = findViewById(R.id.et_name)
+        btnHappy = findViewById(R.id.btn_happy)
+        btnSad = findViewById(R.id.btn_sad)
+        btnMad = findViewById(R.id.btn_mad)
 
         btnHappy.setOnClickListener {
             if (etName.text.isEmpty()) {
                 Toast.makeText(
                     applicationContext,
                     "Please input your name",
-                    Toast.LENGTH_SHORT)
-                    while (etName.text.isNotEmpty()) {
-                    Toast.makeText(
-                        applicationContext,
-                        "Hi $result, You are <happy> today.",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    Toast.LENGTH_SHORT).show()
                     return@setOnClickListener
                 }
-            }
+            Toast.makeText(
+                applicationContext,
+                "Hi " + etName.text + " You are happy today.",
+                Toast.LENGTH_SHORT).show()
+
         }
         btnSad.setOnClickListener {
             if (etName.text.isEmpty()) {
@@ -42,35 +43,30 @@ class MainActivity : AppCompatActivity() {
                     applicationContext,
                     "Please input your name",
                     Toast.LENGTH_SHORT)
-                    while (etName.text.isNotEmpty()) {
-                    Toast.makeText(
-                        applicationContext,
-                        "Hi $result, You are <sad> today.",
-                        Toast.LENGTH_SHORT
-                    ).show()
                     return@setOnClickListener
                 }
-            }
+            Toast.makeText(
+                applicationContext,
+                "Hi " + etName.text + " You are sad today.",
+                Toast.LENGTH_SHORT).show()
 
-        }
+            }
         btnMad.setOnClickListener {
             if (etName.text.isEmpty()) {
                 Toast.makeText(
                     applicationContext,
                     "Please input your name",
                     Toast.LENGTH_SHORT)
-                    while (etName.text.isNotEmpty()) {
-                    Toast.makeText(
-                        applicationContext,
-                        "Hi $result, You are <mad> today.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return@setOnClickListener
-                }
+                return@setOnClickListener
             }
+            Toast.makeText(
+                applicationContext,
+                "Hi " + etName.text + " You are mad today.",
+                Toast.LENGTH_SHORT).show()
         }
 
     }
-}
 
+
+}
 
